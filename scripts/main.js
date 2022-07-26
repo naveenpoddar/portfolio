841c27b2f9448c064f411598ac25320de419894f
Naveen <poddarnaveen321@gmail.com>
2022-07-27 00:04:59 +0530

    contact me fixe

--- scripts/main.js
+++ scripts/main.js
@@ -9,18 +9,30 @@ formEl.addEventListener("submit", async function contactMe(e) {
     return alert("Please fill out all fields");
   }
 
+  const params = new URLSearchParams();
+  params.set("value1", formData.name);
+  params.set("value2", formData.email);
+  params.set("value3", formData.message);
+
   const res = await fetch(
-    "https://maker.ifttt.com/trigger/contact_me/with/key/o7Eta94e_dBOFFC1sNQ0vSbTBzC-x9nixnSJ1DsBS-G",
+    `https://maker.ifttt.com/trigger/contact_me/with/key/o7Eta94e_dBOFFC1sNQ0vSbTBzC-x9nixnSJ1DsBS-G?${params.toString()}`,
     {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        accept: "*/*",
+        "accept-language": "es-ES,es;q=0.9",
+        "content-type": "text/plain",
+        "sec-ch-ua":
+          '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
       },
-      body: JSON.stringify({
-        value1: formData.name,
-        value2: formData.email,
-        value3: formData.message,
-      }),
+      credentials: "omit",
+      mode: "cors",
     }
   );
 
