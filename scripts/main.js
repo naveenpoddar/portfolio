9fb1065cd35f8fb5d91ef36017b926e754fe4530
Naveen <poddarnaveen321@gmail.com>
2022-07-27 00:09:29 +0530

    contact me fixed

--- scripts/main.js
+++ scripts/main.js
@@ -14,31 +14,29 @@ formEl.addEventListener("submit", async function contactMe(e) {
   params.set("value2", formData.email);
   params.set("value3", formData.message);
 
-  const res = await fetch(
-    `https://maker.ifttt.com/trigger/contact_me/with/key/o7Eta94e_dBOFFC1sNQ0vSbTBzC-x9nixnSJ1DsBS-G?${params.toString()}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        accept: "*/*",
-        "accept-language": "es-ES,es;q=0.9",
-        "content-type": "text/plain",
-        "sec-ch-ua":
-          '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
-        "sec-ch-ua-mobile": "?0",
-        "sec-ch-ua-platform": '"Windows"',
-        "sec-fetch-dest": "empty",
-        "sec-fetch-mode": "cors",
-        "sec-fetch-site": "same-origin",
-      },
-      credentials: "omit",
-      mode: "cors",
-    }
-  );
+  try {
+    const res = await fetch(
+      `https://maker.ifttt.com/trigger/contact_me/with/key/o7Eta94e_dBOFFC1sNQ0vSbTBzC-x9nixnSJ1DsBS-G?${params.toString()}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          accept: "*/*",
+          "accept-language": "es-ES,es;q=0.9",
+          "content-type": "text/plain",
+          "sec-ch-ua":
+            '" Not A;Brand";v="99", "Chromium";v="102", "Google Chrome";v="102"',
+          "sec-ch-ua-mobile": "?0",
+          "sec-ch-ua-platform": '"Windows"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-origin",
+        },
+        credentials: "omit",
+        mode: "cors",
+      }
+    );
+  } catch (err) {}
 
-  if (res.ok) {
-    alert("Message sent successfully");
-  } else {
-    alert("Something went wrong");
-  }
+  alert("Message sent successfully");
 });
